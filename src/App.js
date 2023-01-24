6acb428207d667650f747d6ee0db571ded9596e7
Dhananjay <dhananjayrakshe77@gmail.com>
2023-01-24 16:51:23 +0530

    adding auth again n again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
- 
 
 function App() {
   return (
-    <div className="App">
-      <h2>Medvol Admin App</h2>
-      <AmplifySignOut />
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
