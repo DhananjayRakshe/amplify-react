cffa3cf9d4d013f229d5eb4f4b9606a30785724c
Dhananjay <dhananjayrakshe77@gmail.com>
2023-01-24 17:01:01 +0530

    auth done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
+  
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user.username}</h1>
+          {
+            console.log({user: user})
+          }
+          <h1>Medvol Admin</h1>
+          <h3>Username: {user.username}</h3>
+          <h3>JWT token: <i>{user.signInUserSession.accessToken.jwtToken}</i></h3>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
@@ -16,4 +22,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
