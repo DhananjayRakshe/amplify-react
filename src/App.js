9e6a246e9700d855b645b656dada3ef1f8cd6a2d
Dhananjay <dhananjayrakshe77@gmail.com>
2023-01-24 16:42:22 +0530

    adding auth again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { Amplify } from 'aws-amplify';
 
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -8,7 +7,7 @@ import '@aws-amplify/ui-react/styles.css';
 function App() {
   return (
     <div className="App">
-      <h2>Medvol Admin</h2>
+      <h2>Medvol Admin App</h2>
       <AmplifySignOut />
     </div>
   );
